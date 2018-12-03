67954a3346d92b3f73d1ea7968f24125f471eb46
Skylark <37351525+5kyl4rk@users.noreply.github.com>
2018-12-03 13:47:08 -0700

    made label that displays how many times button is pressed

--- src/gui/view/FirstPanel.java
+++ src/gui/view/FirstPanel.java
@@ -14,7 +14,8 @@ public class FirstPanel extends JPanel
 	private JLabel firstLabel;
 	private JButton firstButton;
 	private SpringLayout appLayout;
-	
+	private JLabel buttonCount;
+	private int count;
 	
 	public FirstPanel(GUIController appController)
 	{
@@ -23,22 +24,42 @@ public class FirstPanel extends JPanel
 		this.appController = appController;
 		
 		firstLabel = new JLabel("Words! Wow!");
+		buttonCount = new JLabel("<display count>");
 		firstButton = new JButton("Download Minecraft for FREE!");
 		appLayout = new SpringLayout();
+		
+		count = 0;
 
 		setupPanel();
 		setupLayout();
 		setupListeners();
 	}
 	
+	private String counter()
+	{
+		count++;
+		String displayCount = "You've pressed the button " + count + " times!";
+		
+		return displayCount;
+	}
+	
+	private int randomRGB()
+	{
+		int RGB = (int)(Math.random() * 256);
+		
+		return RGB;
+		
+	}
 	private void changeBackground()
 	{
-		int red = (int)(Math.random() * 256);
-		int green = (int)(Math.random() * 256);
-		int blue = (int)(Math.random() * 256);
+		int red = randomRGB();
+		int green = randomRGB();
+		int blue = randomRGB();
 		
 		this.setBackground(new Color(red, green, blue));
 		firstLabel.setText("Red: " + red + " Green: " + green + " Blue: " + blue);
+		
+		buttonCount.setText(counter());
 	}
 	
 	private void setupPanel() //attach components to frame
@@ -46,17 +67,24 @@ public class FirstPanel extends JPanel
 		this.setLayout(appLayout);
 		this.add(firstButton); //adds components to panel, by default it will stack on top of each other if there isn't a layout
 		this.add(firstLabel);
+		this.add(buttonCount);
+		buttonCount.setVisible(false);
 	}
 	
 	private void setupLayout() //arranges components on frame
 	{
 		firstLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		buttonCount.setHorizontalAlignment(SwingConstants.CENTER);
 		
+		appLayout.putConstraint(SpringLayout.NORTH, buttonCount, 6, SpringLayout.SOUTH, firstLabel);
+		appLayout.putConstraint(SpringLayout.WEST, buttonCount, 0, SpringLayout.WEST, firstButton);
+		appLayout.putConstraint(SpringLayout.EAST, buttonCount, 0, SpringLayout.EAST, firstButton);
 		
 		appLayout.putConstraint(SpringLayout.NORTH, firstButton, 260, SpringLayout.SOUTH, firstLabel);
 		appLayout.putConstraint(SpringLayout.WEST, firstButton, 0, SpringLayout.WEST, firstLabel);
 		appLayout.putConstraint(SpringLayout.SOUTH, firstButton, -50, SpringLayout.SOUTH, this);
 		appLayout.putConstraint(SpringLayout.EAST, firstButton, 0, SpringLayout.EAST, firstLabel);
+		
 		appLayout.putConstraint(SpringLayout.WEST, firstLabel, 100, SpringLayout.WEST, this);
 		appLayout.putConstraint(SpringLayout.EAST, firstLabel, -100, SpringLayout.EAST, this);
 		appLayout.putConstraint(SpringLayout.NORTH, firstLabel, 100, SpringLayout.NORTH, this);
@@ -69,6 +97,7 @@ public class FirstPanel extends JPanel
 		{
 			public void actionPerformed(ActionEvent mouseClick)
 			{
+				buttonCount.setVisible(true);
 				changeBackground();
 			}
 		});
